08159329f9c684342836a9e622a1cccb9a5b1187
binhbn <binhbn@softdreams.vn>
2026-01-13 01:33:17 +0700

    add code 13/01/2026:

--- src/JavaBigDecimal.java
+++ src/JavaBigDecimal.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class JavaBigDecimal {
@@ -5,14 +8,19 @@ public class JavaBigDecimal {
         //Input
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-        String []s=new String[n+2];
-        for(int i=0;i<n;i++){
+        String[] s = new String[n+2];
+        for (int i = 0; i < n; i++){
             s[i]=sc.next();
         }
         sc.close();
-
-        //Write your code here
-
+        Arrays.sort(s, 0, n, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                BigDecimal bd1 = new BigDecimal(a);
+                BigDecimal bd2 = new BigDecimal(b);
+                return bd2.compareTo(bd1);
+            }
+        });
         //Output
         for(int i=0;i<n;i++)
         {
---
+++ src/JavaPrimalityTest.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class JavaPrimalityTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+        bufferedReader.close();
+        BigInteger nInteger = new BigInteger(n);
+        System.out.println(nInteger.isProbablePrime(10) ? "prime" : "not prime");
+    }
+}
